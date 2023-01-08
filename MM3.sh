6eb20b57243b70a2a347be16a5c663279e1b883a
fry-guy <70232344+fry-guy@users.noreply.github.com>
2023-01-08 14:47:38 -0500

    Update MM3.sh

--- MM3.sh
+++ MM3.sh
@@ -25,7 +25,7 @@ sudo /opt/retropie/supplementary/xboxdrv/bin/xboxdrv >> /dev/shm/runcommand.log 
 	--ui-buttonmap tl+dd=KEY_U,tl+dr=KEY_E,tl+du=KEY_D,tl+dl=KEY_R \
         --ui-buttonmap dd+rb=KEY_F,dr+rb=KEY_E+KEY_ENTER:KEY_C:250,du+rb=KEY_N+KEY_C,dl+rb=KEY_A \
         --ui-buttonmap dd+lb=KEY_W+KEY_G+KEY_D,dr+lb=KEY_A+KEY_B+KEY_F,du+lb=KEY_M+KEY_S+KEY_T,dl+lb=KEY_R \
-	--ui-buttonmap dd+b=KEY_E+KEY_U,dr+b=KEY_N+KEY_D:KEY_U+KEY_ENTER:250,du+b=KEY_O,dl+b=KEY_W \
+	--ui-buttonmap dd+b=KEY_E+KEY_U,dr+b=KEY_N+KEY_W:KEY_U+KEY_ENTER:250,du+b=KEY_O,dl+b=KEY_D \
 	--ui-buttonmap dd+y=KEY_S+KEY_T+KEY_H,dr+y=KEY_D+KEY_F,du+y=KEY_I+KEY_U \
 	--ui-buttonmap dd+start=KEY_Y,dd+back=KEY_N,du+start=KEY_ENTER,du+back=KEY_ESC \
         --ui-buttonmap dd+x=KEY_Y,du+x=KEY_N \
