91db1ce47857db862ccbbc919373f21ceec93fb7
fry-guy <70232344+fry-guy@users.noreply.github.com>
2023-02-12 01:07:38 -0500

    Update MM3.sh

--- MM3.sh
+++ MM3.sh
@@ -23,25 +23,25 @@ sudo /opt/retropie/supplementary/xboxdrv/bin/xboxdrv >> /dev/shm/runcommand.log 
 	--ui-buttonmap rb=KEY_C \
 	--ui-buttonmap guide=KEY_LEFTCTRL,tl^click-press=KEY_SPACE,tr^click-press=KEY_F12 \
 	--ui-buttonmap tr+guide=KEY_TAB,back+x=KEY_LEFTALT+KEY_F11 \
-    --ui-buttonmap tr+lb=KEY_F1:KEY_F1:500,tr+y=KEY_F2:KEY_F2:500,tr+x=KEY_F3:KEY_F3:500,tr+rb=KEY_F4:KEY_F4:500,tr+b=KEY_F5:KEY_F5:500,tr+a=KEY_F6:KEY_F6:500,tr+back=KEY_F7:KEY_F7:500,tr+start=KEY_F8:KEY_F8:500 \
+  	--ui-buttonmap tr+lb=KEY_F1:KEY_F1:500,tr+y=KEY_F2:KEY_F2:500,tr+x=KEY_F3:KEY_F3:500,tr+rb=KEY_F4:KEY_F4:500,tr+b=KEY_F5:KEY_F5:500,tr+a=KEY_F6:KEY_F6:500,tr+back=KEY_F7:KEY_F7:500,tr+start=KEY_F8:KEY_F8:500 \
 	--ui-axismap tr+x1=KEY_F10:KEY_F8,tr+y1=KEY_F7:KEY_F9 \
 	--ui-buttonmap tl+lb=KEY_1,tl+y=KEY_2,tl+x=KEY_3,tl+rb=KEY_4,tl+b=KEY_5,tl+a=KEY_6 \
 	--ui-axismap tl+x1=KEY_0+KEY_LEFT:KEY_8+KEY_RIGHT:2000,tl+y1=KEY_7:KEY_9:2000 \
 	--ui-axismap tr+y1=KEY_UP:KEY_DOWN:2000,tr+x1=KEY_LEFT:KEY_RIGHT:2000 \
 	--ui-buttonmap tr+tl=KEY_RIGHTCTRL \
-    --ui-buttonmap dr+rb=KEY_ENTER+KEY_C,du+rb=KEY_ESC+KEY_C+KEY_N \
+    	--ui-buttonmap dr+rb=KEY_ENTER+KEY_C,du+rb=KEY_ESC+KEY_C+KEY_N \
 	--ui-buttonmap lb+dd=KEY_F,lb+dr^click-press=KEY_E,lb+du=KEY_C,lb+dl=KEY_A \
-    --ui-buttonmap dd+lb=KEY_G+KEY_B+KEY_D,dr+lb=KEY_B+KEY_F,du+lb=KEY_S+KEY_I+KEY_T,dl+lb=KEY_R \
+    	--ui-buttonmap dd+lb=KEY_G+KEY_B+KEY_D,dr+lb=KEY_B+KEY_F,du+lb=KEY_S+KEY_I+KEY_T,dl+lb=KEY_R \
 	--ui-buttonmap dd+b^click-press=KEY_U,dr+b=KEY_N:KEY_ENTER:200,du+b=KEY_O \
 	--ui-buttonmap dd+x^click-press=KEY_O+KEY_Y,dr+x=KEY_W,du+x^click-press=KEY_E+KEY_N,dl+x=KEY_D \
 	--ui-buttonmap dd+y=KEY_S+KEY_W+KEY_T+KEY_H:KEY_Y:750,dr+y=KEY_D+KEY_A+KEY_F:KEY_Y:750,du+y=KEY_M+KEY_U+KEY_I:KEY_Y:750 \
 	--ui-buttonmap tl+dr=KEY_S+KEY_E:KEY_U:750,tl+dl=KEY_R:KEY_D:750 \
-    --ui-buttonmap tl+du^auto:250:250=cycle-key-named:list:KEY_1:KEY_2:KEY_3:KEY_4:KEY_5:KEY_6:KEY_7:KEY_8:KEY_9:KEY_0 \
-    --ui-buttonmap tl+dd^auto:250:250=cycle-key-ref:list:backward \
-    --ui-buttonmap tr+dd=KEY_S:KEY_B,tr+du=KEY_R:KEY_D+KEY_I \
-    --ui-buttonmap y+dd=KEY_S:KEY_Y:750,y+dr=KEY_F:KEY_Y:750,y+du=KEY_I:KEY_Y:750 \
-    --ui-buttonmap tr+dr^auto:250:250=cycle-key-named:characters:KEY_F1:KEY_F2:KEY_F3:KEY_F4:KEY_F5:KEY_F6 \
-    --ui-buttonmap tr+dl^auto:250:250=cycle-key-ref:characters:backward \
+    	--ui-buttonmap tl+du^auto:250:250=cycle-key-named:list:KEY_1:KEY_2:KEY_3:KEY_4:KEY_5:KEY_6:KEY_7:KEY_8:KEY_9:KEY_0 \
+    	--ui-buttonmap tl+dd^auto:250:250=cycle-key-ref:list:backward \
+    	--ui-buttonmap tr+dd=KEY_S:KEY_B,tr+du=KEY_R:KEY_D+KEY_I \
+    	--ui-buttonmap y+dd=KEY_S:KEY_Y:750,y+dr=KEY_F:KEY_Y:750,y+du=KEY_I:KEY_Y:750 \
+    	--ui-buttonmap tr+dr^auto:250:250=cycle-key-named:characters:KEY_F1:KEY_F2:KEY_F3:KEY_F4:KEY_F5:KEY_F6 \
+    	--ui-buttonmap tr+dl^auto:250:250=cycle-key-ref:characters:backward \
 	--ui-buttonmap b+y=macro:/home/pi/RetroPie/roms/ports/xboxdrv_macros/wizeye.macro \
 	--ui-buttonmap guide+lb=macro:/home/pi/RetroPie/roms/ports/xboxdrv_macros/home.macro \
 	--ui-buttonmap guide+y=macro:/home/pi/RetroPie/roms/ports/xboxdrv_macros/seadog.macro \
